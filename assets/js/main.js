73fffb14231609339c8388e9e10ea3cc9aa08905
丁希虎 <dingxihu@zhiketong.cn>
2023-10-08 15:26:04 +0800

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,7 +170,8 @@
 		'click',
 		'.scrollto',
 		function (e) {
-			if (select(this.hash)) {
+			console.log(121212,this.hash);
+			if (this.hash && select(this.hash)) {
 				e.preventDefault();
 
 				let navbar = select('#navbar');
@@ -210,30 +211,19 @@
 	/**
 	 * Testimonials slider
 	 */
-	new Swiper('.testimonials-slider', {
+	new Swiper('.banner-slider', {
 		speed: 600,
 		loop: true,
-		autoplay: {
-			delay: 5000,
-			disableOnInteraction: false,
-		},
+		// autoplay: {
+		// 	delay: 5000,
+		// 	disableOnInteraction: false,
+		// },
 		slidesPerView: 'auto',
-		pagination: {
-			el: '.swiper-pagination',
-			type: 'bullets',
-			clickable: true,
-		},
-		breakpoints: {
-			320: {
-				slidesPerView: 1,
-				spaceBetween: 40,
-			},
-
-			1200: {
-				slidesPerView: 3,
-				spaceBetween: 40,
-			},
-		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		  },
+	
 	});
 
 	/**
