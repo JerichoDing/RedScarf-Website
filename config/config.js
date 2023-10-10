3290fe05a5bbc673d30e7f73df543918d6fdafbf
丁希虎 <dingxihu@zhiketong.cn>
2023-10-10 09:21:14 +0800

    add nodejs server

---
+++ config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    wechat: {
+        appID: "wx0c3b2908a69d772b", // 星月
+        appsecret: "fa2c4ce00037ea714c01ecf83459367f",// 星月appsecret
+        token: "adingmoon",
+        url: "http://www.redscarfappeal.com",
+
+
+        // EncodingAESKey:'hlDeN6a49F98jig8KZzQRhWm969xENh77EeMsybq2ns'
+
+    }
+}
