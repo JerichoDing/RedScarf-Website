7c5a758c517bfb74bd9559cab3bb07733a96599b
丁希虎 <dingxihu@zhiketong.cn>
2023-10-12 02:36:08 +0800

    111

--- reply/reply.js
+++ reply/reply.js
@@ -18,14 +18,9 @@ module.exports = async (message) => {
 		options.mediaId = message.MediaId;
 		console.log(message.Recognition);
 	} else if (message.MsgType === 'event') {
+		console.log('Event',message);
 		if (message.Event === 'subscribe') {
-			replyBody =
-				'欢迎您的关注~\n' +
-				'回复 首页 查看电影预告片页面\n' +
-				'回复 热门 查看最新热门电影\n' +
-				'回复 文本 查看指定电影信息\n' +
-				'回复 语音 查看指定电影信息\n' +
-				'也可以点击下面的菜单按钮来了解其它信息~';
+			replyBody = '欢迎您的关注~\n' ;
 		} else if (message.Event === 'unsubscribe') {
 			console.log('用户取消关注了！');
 		} else if (message.Event === 'CLICK') {
