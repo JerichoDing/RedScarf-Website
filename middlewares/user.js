98d5131333878d2daf11168501428c6a94c1c9cb
丁希虎 <dingxihu@zhiketong.cn>
2023-10-19 18:20:29 +0800

    12

--- middlewares/user.js
+++ middlewares/user.js
@@ -28,56 +28,56 @@ const checkUserByCookieId = async (ctx, uuid) => {
 };
 
 module.exports = async (ctx, next) => {
-	if (ctx.url.indexOf('assets') > -1) {
-		return next();
-	}
-	console.log('是否微信环境----', Tool.isWxBrowser(ctx), ctx.url);
-	const callbackUrl = `${domain}${ctx.url}`;
-	const cookieId = UserTools.getCookie(ctx, 'uuid');
-	const uuid = UserTools.getUserDeviceInfo(ctx);
-	if (cookieId) {
-		const isUserExist = await checkUserByCookieId(ctx, cookieId);
+	if (ctx.url.indexOf('assets') < 0) {
+		console.log('是否微信环境----', Tool.isWxBrowser(ctx), ctx.url);
+		const callbackUrl = `${domain}${ctx.url}`;
+		const cookieId = UserTools.getCookie(ctx, 'uuid');
+		const uuid = UserTools.getUserDeviceInfo(ctx);
+		if (cookieId) {
+			const isUserExist = await checkUserByCookieId(ctx, cookieId);
+			if (isUserExist) {
+				return next();
+			}
+		}
+		// 没有cookie 或者cookie失效的情况 start
+	
+		// 微信用户组注册逻辑
+		if (Tool.isWxBrowser(ctx)) {
+			const { code } = ctx.query;
+			if (code) {
+				try {
+					let result = await wechatApi.getOauthAccessToken(code);
+					let data = await wechatApi.getOauthUserinfo(
+						result.access_token,
+						result.openid
+					);
+					// 创建微信用户
+					await createUserAndSetCookie(ctx, {
+						openid: data.openid,
+						unionid: data.unionid,
+						name: data.nickname,
+						avatar: data.headimgurl,
+					});
+					let url = fancyURI.removeSearch('code', callbackUrl);
+					// state 目前没有用到
+					url = fancyURI.removeSearch('state', url);
+					ctx.redirect(`${url}`);
+					return next();
+				} catch (error) {}
+			} else {
+				ctx.redirect(`/getUserInfo?backurl=${encodeURIComponent(callbackUrl)}`);
+				return next();
+			}
+		}
+		// 判断该用户是否在数据库中
+		const isUserExist = await checkUserByCookieId(ctx, uuid);
 		if (isUserExist) {
 			return next();
 		}
-	}
-	// 没有cookie 或者cookie失效的情况 start
-
-	// 微信用户组注册逻辑
-	if (Tool.isWxBrowser(ctx)) {
-		const { code } = ctx.query;
-		if (code) {
-			try {
-				let result = await wechatApi.getOauthAccessToken(code);
-				let data = await wechatApi.getOauthUserinfo(
-					result.access_token,
-					result.openid
-				);
-				// 创建微信用户
-				await createUserAndSetCookie(ctx, {
-					openid: data.openid,
-					unionid: data.unionid,
-					name: data.nickname,
-					avatar: data.headimgurl,
-				});
-				let url = fancyURI.removeSearch('code', callbackUrl);
-				// state 目前没有用到
-				url = fancyURI.removeSearch('state', url);
-				ctx.redirect(`${url}`);
-				return next();
-			} catch (error) {}
-		} else {
-			ctx.redirect(`/getUserInfo?backurl=${encodeURIComponent(callbackUrl)}`);
-			return next();
-		}
-	}
-	// 判断该用户是否在数据库中
-	const isUserExist = await checkUserByCookieId(ctx, uuid);
-	if (isUserExist) {
+		// 创建非微信用户
+		await createUserAndSetCookie(ctx, { unionid: uuid });
 		return next();
 	}
-	// 创建非微信用户
-	await createUserAndSetCookie(ctx, { unionid: uuid });
-	return next();
+
 	// 没有cookie 或者cookie失效的情况 end
 };
